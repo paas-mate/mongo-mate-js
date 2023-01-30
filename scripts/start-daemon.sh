e5890543f8c219d5f19c2c0ccb8ace9f2e3a2857
Lico-Tom <583566138@qq.com>
2023-01-30 20:52:08 +0800

    start mongo

--- scripts/start-daemon.sh
+++ scripts/start-daemon.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
+MONGO_MATE="${MONGO_HOME}/mate"
+MONGO_SCRIPTS="${MONGO_MATE}/scripts"
+START_STANDALONE_SCRIPT="${MONGO_SCRIPTS}/start-mongo-stand-alone.sh"
+START_CONFIG_SCRIPT="${MONGO_SCRIPTS}/start-mongo-config.sh"
+START_ROUTER_SCRIPT="${MONGO_SCRIPTS}/start-mongo-router.sh"
+START_SHARD_SCRIPT="${MONGO_SCRIPTS}/start-mongo-shard.sh"
+
 mkdir -p $MONGO_HOME/logs
+
+case ${DEPLOY_TYPE} in
+"stand-alone")
+  echo "start stand alone mongo."
+  bash -xv ${START_STANDALONE_SCRIPT}
+  ;;
+"config")
+  echo "start mongo config."
+  bash -xv ${START_CONFIG_SCRIPT}
+  ;;
+"router")
+  echo "start mongo router."
+  bash -xv ${START_ROUTER_SCRIPT}
+  ;;
+"shard")
+  echo "start mongo shard."
+  bash -xv ${START_SHARD_SCRIPT}
+  ;;
+esac
+
